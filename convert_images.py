938f1893b2bdf6f756b78bdbebca7991ecde46c2
jongok <bjo9280@gmail.com>
2018-11-20 15:06:57 +0900

    first commit

---
+++ convert_images.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import vis
+from scipy.misc import imread
+import os
+import numpy as np
+import re
+import cv2
+
+#change the name of output images when option is --also_save_raw_predictions=True
+def dequote(fname):
+    fname = os.path.splitext(fname)[0]
+    parse = re.sub('[b]', '', fname) #remove b
+
+    if(parse[0] == parse[-1]) and parse.startswith(("'",'"')):
+        return parse[1:-1]
+    return parse
+
+
+def convert_grayscale_to_rgb(folder, num_classes):
+    for root, dirs, files in os.walk(folder):
+        for fname in files:
+            im = imread(folder+fname)
+            voc_palette = vis.make_palette(num_classes)
+            out_im = Image.fromarray(vis.color_seg(im, voc_palette))
+            print(dequote(fname))
+            out_im.save("{}{}_prediction.png".format(folder, dequote(fname)))
+
+def convert_rgb_to_grayscale(folder):
+    for root, dirs, files in os.walk(folder + 'SegmentationClassPNG'):
+        for fname in files:
+            im = Image.open(folder + "SegmentationClassPNG/" + fname)
+            in_ = np.array(im, dtype=np.float32)
+            stacked_img = np.stack((in_,) * 3, -1)
+            cv2.imwrite(folder + "SegmentationClassAug/{}".format(fname), stacked_img)
+
